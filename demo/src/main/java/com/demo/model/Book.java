bc7d7410fefb62461e1225f884c2a82ce49eddf9
mait <talantm89@gmail.com>
2024-04-02 17:59:22 +0300

    Added @ToString annotation

--- demo/src/main/java/com/demo/model/Book.java
+++ demo/src/main/java/com/demo/model/Book.java
@@ -9,10 +9,12 @@ import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 @Entity
 public class Book {
     @Id
